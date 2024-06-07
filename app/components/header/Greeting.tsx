b5da30f45fe6abe46f5dcecd4e51b85bf4b67796
tomaszbilka <bila4@op.pl>
2024-06-07 11:07:29 +0200

    add header

---
+++ app/components/header/Greeting.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import Typewriter from "typewriter-effect";
+
+const Greeting = () => {
+  const [isAnimationFinished, setIsAnimationFinished] = useState(false);
+  const { t } = useTranslation();
+
+  const frontEnd = "Front-end developer.";
+  const fullStack = "Full-stack developer.";
+
+  return (
+    <div className="greeting">
+      <div>
+        <span>{t("hello")}</span>
+        <span className="greeting__name">Tomasz Bilka</span>
+      </div>
+      <div className="greeting__role">
+        {isAnimationFinished ? (
+          frontEnd
+        ) : (
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(500)
+                .typeString(fullStack)
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString(frontEnd)
+                .callFunction(() => setIsAnimationFinished(true))
+                .start();
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Greeting;
--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { TbArrowsDown } from "react-icons/tb";
 import { useTranslation } from "react-i18next";
 
+import Greeting from "./Greeting";
 import LanguageSwitch from "../languageSwitch";
 
 const Header = () => {
@@ -8,6 +9,7 @@ const Header = () => {
 
   return (
     <div className="my-header">
+      <Greeting />
       <div role="button" className="my-header__container">
         <div className="my-header__title">{t("exploreMore")}</div>
         <div className="my-header__arrow-container">
