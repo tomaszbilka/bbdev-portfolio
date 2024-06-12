69d2ce7120d212ca5b041bbd1892bbff1021f452
tomaszbilka <bila4@op.pl>
2024-06-12 08:31:15 +0200

    add contact section

--- app/i18n/locales/pl/index.ts
+++ app/i18n/locales/pl/index.ts
@@ -1,9 +1,11 @@
 import about from "./about.json";
 import common from "./common.json";
+import contact from "./contact.json";
 import skills from "./skills.json";
 
 export default {
   about,
   common,
+  contact,
   skills,
 };
