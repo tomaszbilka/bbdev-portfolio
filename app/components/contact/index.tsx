7c9702d8b0a7f85279f8064141ddc683e9a4b69a
Tomasz <74007928+tomaszbilka@users.noreply.github.com>
2024-06-30 14:49:26 +0200

    small fixes (#10)

--- app/components/contact/index.tsx
+++ app/components/contact/index.tsx
@@ -1,56 +1,46 @@
-import { useTranslation } from "react-i18next";
-import {
-  FaChartLine,
-  FaGithub,
-  FaLinkedin,
-  FaProjectDiagram,
-  FaUniversity,
-} from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
+import { TbArrowsDown } from "react-icons/tb";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
   const { t } = useTranslation("contact");
   return (
     <section className="contact-section">
       <div className="contact">
-        <div className="contact__info">
-          <div className="contact__info__wrapp">
-            <FaUniversity />
-            <span>{t("knowledge")}</span>
-          </div>
-          <div className="contact__info__wrapp">
-            <FaProjectDiagram />
-            <span>{t("flexibility")}</span>
-          </div>
-          <div className="contact__info__wrapp">
-            <FaChartLine />
-            <span>{t("growth")}</span>
-          </div>
-        </div>
         <div className="contact__texts">
           <h1>{t("contact")}</h1>
-          <p>{t("info")}</p>
-        </div>
-        <div>
-          <a
-            className="contact__icon"
-            href="https://www.linkedin.com/in/tomasz-bi%C5%82ka-bbdev/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FaLinkedin className="icon icon__linkedin rotate" />
-          </a>
-          <a
-            className="contact__icon"
-            href="https://github.com/tomaszbilka"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FaGithub className="icon icon__github rotate" />
-          </a>
-          <a href="mailto:tomaszbilka@gmail.com" className="contact__icon">
-            <SiGmail className="icon icon__gmail rotate" />
-          </a>
+          <div className="contact__text">
+            <p>{t("info")}</p>
+            <TbArrowsDown className="arrows" />
+          </div>
+          <div className="contact__socials">
+            <a
+              className="contact__icon"
+              href="https://www.linkedin.com/in/tomasz-bi%C5%82ka-bbdev/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FaLinkedin className="icon icon__linkedin rotate" />
+            </a>
+            <a
+              className="contact__icon"
+              href="https://github.com/tomaszbilka"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FaGithub className="icon icon__github rotate" />
+            </a>
+          </div>
+          <div className="contact__text">
+            <p>{t("emailMe")}</p>
+            <TbArrowsDown className="arrows" />
+          </div>
+          <div>
+            <a href="mailto:tomaszbilka@gmail.com" className="contact__icon">
+              <SiGmail className="icon icon__gmail rotate" />
+            </a>
+          </div>
         </div>
       </div>
       <footer className="footer">
